1057be575cd130b5b0957939b5cc6a8352db59a9
buuug7 <youpp@126.com>
2016-10-12 18:36:28 +0800

    首页添加首页活动栏目，广告位

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -6,5 +6,35 @@
 'use strict';
 
 $(document).ready(function () {
-  //window.alert("diqiue");
+
+  // 首页活动滚动
+  var $owlHuodongV1 = $('.huodong-v1-slide');
+  $owlHuodongV1.owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      750: {
+        items: 2,
+        nav: false,
+      },
+      970: {
+        items: 4,
+        nav: true,
+        loop: false,
+      },
+    },
+  });
+  $('.prev-v1').click(function () {
+    $owlHuodongV1.trigger('prev.owl.carousel');
+  });
+
+  $('.next-v1').click(function () {
+    $owlHuodongV1.trigger('next.owl.carousel');
+  });
+
 });
